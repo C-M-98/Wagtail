10d3a106b9bb97daf79ec58ba9348ecb61fdf72c
Christian Meintjes <cmeintjes1998@gmail.com>
2024-11-08 11:00:47 +0200

    Start project

---
+++ blog/blog/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-i(vh98s=evgqlb)wwt^sa+y5wj&$7ndlx1e))p)fd(9048!3hy"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
